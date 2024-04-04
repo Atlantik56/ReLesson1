e20a8aa09550b03b5f1d2727730bf1fc7cdd6f04
Atlantik56 <avatar.aang5615@yandex.ru>
2024-04-04 20:45:36 +0300

    Итоговая работа в файле main.js и app.jsx app.cc

---
+++ my-app/src/App.jsx
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1 className ="title">Рабочие дни недели</h1>
+
+    <ol className ="orderList">
+    <li className ="orderPosition">Понедельник</li>
+    <li className ="orderPosition">Вторник</li>
+    <li className ="orderPosition">Среда</li>
+    <li className ="orderPosition">Четверг</li> 
+    <li className ="orderPosition">Пятница</li>
+    </ol>
+    
+    
+    
+    </>
+  );
+}
+
+export default App;
